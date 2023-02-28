8ffc7169b173901b018e417f8f0e63a87ca00c4f
Michaelmc2005 <michaelmc2005@gmail.com>
2023-02-28 19:27:13 +0000

    fully working, clicking on a node fills txtblock

--- script.js
+++ script.js
@@ -54,13 +54,13 @@ function centerContainer() {
   container.style.transition = 'transform 0.5s ease';
   container.style.transform = 'translate(0, 0)';
 }
-
+var chooseEXPAND = 0;
 centralNode.addEventListener('click', () => {
   centerContainer();
 });
-
 surroundingNodes.forEach(node => {
   node.addEventListener('click', () => {
+    
 
     const nodePosition = node.getBoundingClientRect();
     const nodeX = nodePosition.x + nodePosition.width / 2;
@@ -70,14 +70,82 @@ surroundingNodes.forEach(node => {
     const centralNodeX = centralNodePosition.x + centralNodePosition.width / 2;
     const centralNodeY = centralNodePosition.y + centralNodePosition.height / 2;
 
+    
     const distanceX = centralNodeX - nodeX;
     const distanceY = centralNodeY - nodeY;
 
     const container = document.querySelector('.container');
     container.style.transition = 'transform 0.5s ease';
     container.style.transform = `translate(${distanceX}px, ${distanceY}px)`;
-  });
-});
+    const prompt = "a paragraph about" + node.textContent + " that gives concise and useful information and any necessary context"
+    
+    const url = "https://api.openai.com/v1/completions"
+    const apiKey = "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"
+
+
+    const headers = {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${apiKey}`
+    }
+    if(node.classList.contains("node-1")) {
+      chooseEXPAND = 0;
+    } else if(node.classList.contains("node-2")) {
+      chooseEXPAND = 1;
+    }
+    else if(node.classList.contains("node-3")){
+      chooseEXPAND = 2;
+    }
+    else if(node.classList.contains("node-4")){
+      chooseEXPAND = 3;
+    }
+    else if(node.classList.contains("node-5")){
+      chooseEXPAND = 4;
+    }
+    else if(node.classList.contains("node-6")){
+      chooseEXPAND = 5;
+    }
+
+    const requestBody = JSON.stringify({
+      prompt: prompt,
+      model: "text-davinci-003",
+      max_tokens: 100,
+      n: 1,
+      temperature: 0.5 // Change this value to adjust the temperature
+    })
+
+    console.log("Sending request to OpenAI API...")
+    fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: requestBody
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log("Received response from OpenAI API:", data)
+      const result = data.choices[0].text
+      console.log(result) 
+      
+      const dataArray = result.split("\n"); // split the string into an array of substrings
+
+      while (dataArray.length > 0 && dataArray[0].trim() === "") {
+        dataArray.shift();
+      }
+
+      for (let i = 0; i < dataArray.length; i++) {
+        dataArray[i] = dataArray[i].replace(/^\d+\./, "").trim();
+      }
+      
+      surroundingexpandNode[chooseEXPAND].textContent = dataArray;  
+
+      
+    })
+    .catch(error => {
+      console.error("Error from OpenAI API:", error)
+    })
+
+      }
+  );
+
 
 
 
@@ -112,7 +180,7 @@ inputField.addEventListener('keypress', function (e) {
     const prompt = "subtopics about" + inputField.value + "that appear in a list one after another with six elements in the list"
     
     const url = "https://api.openai.com/v1/completions"
-    const apiKey = "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"
+    const apiKey = "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"
 
 
     const headers = {
@@ -164,4 +232,4 @@ inputField.addEventListener('keypress', function (e) {
 
       }
       
-});
+});})
