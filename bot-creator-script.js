68198d7c8d3d20fdebb88c4e074b6d9c65615e3f
Michaelmc2005 <michaelmc2005@gmail.com>
2023-04-27 12:15:02 +0100

    commit2

--- bot-creator-script.js
+++ bot-creator-script.js
@@ -16,7 +16,7 @@
           }
 
     const extracted = document.getElementById('outputText').textContent
-    const prompt = `You are socratique, an educational AI assistant, your primary role is to help the user learn by using the socratic method of questioning and validating their answers, you must only center primarily around the topic that you are provided with initally. If there is a document provided, make references to it and also allow the user to question certain parts of it, remember to respond in the provided tone aswell: Tone - ${tone}; topic - ${interests} and also if there is a document provided after this text, ensure you ask questions about the contents of the following document: ${extracted}`;
+    const prompt = `You are socratique, an educational AI assistant, your primary role is to help the user learn by using the socratic method of questioning and validating their answers, you must only center primarily around the topic that you are provided with initally. If there is a document provided, make references to it and also allow the user to question certain parts of it, remember to respond in the provided tone aswell: Tone - ${tone}; topic - ${interests} and also if there is a document provided in this prompt, ensure you ask questions about the contents of the following 'document': ${extracted}`;
     // Store the prompt in localStorage
     const chatbotList = JSON.parse(localStorage.getItem("chatbots")) || [];
     chatbotList.push({ name: `${tone} - ${interests}`, prompt: prompt });
