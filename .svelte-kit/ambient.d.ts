5f68f70b891b5fd3b53dbaab03660bc5bf529c98
Michael <michaelmc2005@gmail.com>
2023-08-14 23:18:02 +0100

    paid invoice on api lol

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -26,6 +26,7 @@
  * ```
  */
 declare module '$env/static/private' {
+	export const OPENAI_KEY: string;
 	export const MANPATH: string;
 	export const TERM_PROGRAM: string;
 	export const NODE: string;
@@ -116,6 +117,7 @@ declare module '$env/static/public' {
  */
 declare module '$env/dynamic/private' {
 	export const env: {
+		OPENAI_KEY: string;
 		MANPATH: string;
 		TERM_PROGRAM: string;
 		NODE: string;
